69de1e278f61477aca431c3e58dac9bb644891e5
kmk4162 <kmk41623@gmail.com>
2022-11-04 23:44:20 +0900

    Create grandchildren app

--- pjt/urls.py
+++ pjt/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('emotions.urls')),
+    path('grandchildren/', include('grandchildren.urls')),
 ]
